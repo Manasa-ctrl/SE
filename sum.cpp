091ffaaf328da32c40b6a8d9ab3fa56aa4e8e257
Manasa-ctrl <saimanasag17@gmail.com>
2025-02-04 09:49:17 +0530

    Update sum.cpp

--- sum.cpp
+++ sum.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() {
-    int num1, num2, sum;
+    int a, b, sum;
 
     cout << "Enter the first number: ";
     cin >> num1;
@@ -10,7 +10,7 @@ int main() {
     cout << "Enter the second number: ";
     cin >> num2;
 
-    sum = num1 - num2;
+    sum = num1 + num2;
 
     cout << "The sum of " << num1 << " and " << num2 << " is " << sum << "." << endl;
 
